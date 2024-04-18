70822df65cb977769c0429b039aa89c08a553c3d
Hbaba2310 <148599980+Hbaba2310@users.noreply.github.com>
2024-04-18 01:25:31 -0700

    Add files via upload
    Async with practice code

--- practice.js
+++ practice.js
@@ -1,6 +1,6 @@
 // Asynchronous JS is a programming process that allows for the program to run a heavy task while allowing for the program to be able to perform other commands/actions
 
-const log = document.querySelector(".event-log");
+/*const log = document.querySelector(".event-log");
 
 document.querySelector("#xhr").addEventListener("click", () => {
   log.textContent = "";
@@ -17,6 +17,35 @@ document.querySelector("#xhr").addEventListener("click", () => {
   );
   xhr.send();
   log.textContent = `${log.textContent}Started XHR request\n`;
+});
+
+document.querySelector("#reload").addEventListener("click", () => {
+  log.textContent = "";
+  document.location.reload();
+}); */
+
+const log = document.querySelector(".event-log");
+
+document.querySelector("#fetch").addEventListener("click", async () => {
+  log.textContent = "";
+
+  try {
+    log.textContent = `${log.textContent}Started Fetch request\n`;
+
+    const response = await fetch(
+      "https://raw.githubusercontent.com/mdn/content/main/files/en-us/_wikihistory.json"
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    log.textContent = `${log.textContent}Finished Fetch request\n`;
+  } catch (error) {
+    log.textContent = `${log.textContent}Error: ${error.message}\n`;
+  }
 });
 
 document.querySelector("#reload").addEventListener("click", () => {
